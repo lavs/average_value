79df4a1a767beecd3e0ec2dce1f4eed7c508b1fd
lavs <47961813+lavs@users.noreply.github.com>
2019-04-03 12:41:07 +0700

    refactoring code

--- avarage_value-code.py
+++ avarage_value-code.py
@@ -1,23 +1,10 @@
 def dictUpdateFunction(strText):
-    lisText = []
-    for key in range(1, 5):
-            list_key = int(key)                      #   ключ для словаря значений
-            list_value = float(strText[key])          #   тут значение для словаря
-            dic[list_key].append(list_value)        #   добавление значения в словарь
-            dic[list_key].pop(0)
-            sumList = 0
+    for key in range(1, 5):     # key - номер столбца в файлах
+        list_value = float(strText[key])    #list_value - строка в исходном файле
+        dic[key].append(list_value)        #накопление количества строк, необходимого для усреднения
 
-            for i in dic[list_key]:
-                sumList += float(i)
-                print('sumList', sumList)
-            lisText.append(str(sumList / n))
-            print('lisText', lisText)
-
-    print('Итог: ', lisText)
-
-    return lisText
-
-"---------------------------------------------------"
+    print('dic: ', dic)
+    return dic
 
 numPP = 1        #№ п/п
 n = 0           #число усреднения строк
@@ -26,10 +13,7 @@ while n <= 1:
         n = int(input())
 
 dic = {a: ['-'] for a in range(1, 5)}
-
-
 work_file = open("D:\\tmp\\outSNGS.txt", "r")     #читаем файл
-#work_file = open("D:\\tmp\\text_test.txt", "r")     #читаем файл
 final_file = open("D:\\tmp\\final_text.txt", "w")   #сюда пишем результат
 
 for i in work_file:
@@ -37,25 +21,34 @@ for i in work_file:
         print('Исходные значения: ', strText)
         if i.count('"') < 3:    #заголовки в заключены в "", но "" присутствуют так же в первом столбце - датах
             if numPP >= n:      #усредняем значения и выводим результат
-
                     i = 0
-                    final_file.write('    '.join(dictUpdateFunction(strText)))
-                    final_file.write('\n')
-                    numPP += 1
-            else:       #подготовка данных к усреднению
+                    dictUpdateFunction(strText)
                     for key in range(1, 5):
-                            list_key = int(key)                      #   ключ для словаря значений
-                            list_value = float(strText[key])          #   тут значение для ключа
-                            dic[list_key].append(list_value)        #   добавление значения в словарь
-                            print(dic[list_key][0], end='       ')
-                            final_file.write(dic[list_key][0])
-                    final_file.write('\n')
+                        dic[key].pop(0)     #удаление строки, входящей в предыдущий диапазон вычислений
+                        sumList = 0
 
-                    print()
+                        for i in dic[key]:
+                            sumList += float(i)
+                            print('sumList', sumList)
+
+                        medium = sumList / n        #среднее значение
+                        print('Итог: ', str(medium))
+                        final_file.write(str(medium) + '    ') # запись среднего значения в файл
+
+                    final_file.write('\n')   #перевод строки в файле
                     numPP += 1
+
+            else:   #подготовка данных к усреднению, накопление значений в словарь
+                    dictUpdateFunction(strText)
+                    for key in range(1, 5):
+                        print(dic[key][0], end='       ')
+                        final_file.write(dic[key][0]) # запись в файл значений по умолчанию, в данном случае это "-"
+                    final_file.write('\n')
+                    print()
+                    numPP += 1  #счётчик строк, взятых в работу с исходного файла
         else:
             final_file.write(''.join(str(n)))
             final_file.write('\n')
 
-work_file.close()
+work_file.close()   #закрытие рабочего и итогового файлов
 final_file.close()
